cef9e274028ce12d37c6903c81cd8a4bfa1edd6b
Francerz <francerzam@hotmail.com>
2023-01-12 11:11:54 -0600

    ADD AbstractSession class

---
+++ src/AbstractSession.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Francerz\WebappCommons;
+
+use RuntimeException;
+
+abstract class AbstractSession
+{
+    /**
+     * @throws RuntimeException
+     */
+    public static function start()
+    {
+        switch (session_status()) {
+            case PHP_SESSION_NONE:
+                if (!headers_sent()) {
+                    throw new RuntimeException('Session start failed! Headers already sent.');
+                }
+                session_start();
+                break;
+            case PHP_SESSION_DISABLED:
+                throw new RuntimeException('Sessions start failed! Sessions disabled.');
+        }
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public static function set(string $key, $value)
+    {
+        static::start();
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public static function get(string $key)
+    {
+        static::start();
+        return $_SESSION[$key] ?? null;
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public static function unset(string $key)
+    {
+        $val = static::get($key);
+        unset($_SESSION[$key]);
+        return $val;
+    }
+
+    public static function destroy()
+    {
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            return;
+        }
+        session_destroy();
+    }
+}
