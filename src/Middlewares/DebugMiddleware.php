8f6c879fa8c61a5b93355c20fe98bea9ffcbaa6c
Francerz <francerzam@hotmail.com>
2023-07-14 11:29:12 -0600

    ADD Debug Environments

--- src/Middlewares/DebugMiddleware.php
+++ src/Middlewares/DebugMiddleware.php
@@ -14,18 +14,36 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class DebugMiddleware implements MiddlewareInterface
 {
+    public const ENV_PRODUCTION = 'production';
+    public const ENV_DEVELOPER = 'developer';
+
     private $responseFactory;
+    private $environment = self::ENV_PRODUCTION;
 
     public function __construct(ResponseFactoryInterface $responseFactory)
     {
         $this->responseFactory = $responseFactory;
     }
+
+    public function setEnvironment(string $environment)
+    {
+        $this->environment = $environment;
+    }
+
+    private function initDebugContext()
+    {
+        if (in_array($this->environment, [self::ENV_DEVELOPER])) {
+            error_reporting(E_ALL);
+            ini_set('display_errors', 1);
+        }
+    }
+
     private function setErrorHandler()
     {
         set_error_handler(function ($errno, $error, $errfile, $errline) {
-            if (php_sapi_name() !== 'cli-server') {
-                return;
-            }
+            // if (php_sapi_name() !== 'cli-server') {
+            //     return;
+            // }
             throw new ErrorException("{$errno}: {$error} ({$errfile}:{$errline})", $errno);
         });
     }
@@ -33,12 +51,17 @@ class DebugMiddleware implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         try {
+            $this->initDebugContext();
             $this->setErrorHandler();
             return $handler->handle($request);
         } catch (Exception $ex) {
+            $errorString = get_class($ex) . ': ' . $ex->getMessage();
             $response = $this->responseFactory->createResponse(500);
+            if (in_array($this->environment, [self::ENV_DEVELOPER])) {
+                $response = $response->withHeader('Content-Type', 'text/plain');
+                $response->getBody()->write($errorString);
+            }
             if (php_sapi_name() === 'cli-server') {
-                $errorString = get_class($ex) . ': ' . $ex->getMessage();
                 error_log(
                     BackColors::RED .
                     ForeColors::WHITE .
@@ -46,8 +69,6 @@ class DebugMiddleware implements MiddlewareInterface
                     BackColors::DEFAULT .
                     ForeColors::DEFAULT
                 );
-                $response = $response->withHeader('Content-Type', 'text/plain');
-                $response->getBody()->write($errorString);
             }
             return $response;
         }
